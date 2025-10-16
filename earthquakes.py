e206a7187e7d23e840336b7558893ccac333ab7c
Yuchen Gu <zczq773@ucl.ac.uk>
2025-10-16 23:25:18 +0800

    change

--- earthquakes.py
+++ earthquakes.py
@@ -1,13 +1,14 @@
-# The Python standard library includes some functionality for communicating
-# over the Internet.
-# However, we will use a more powerful and simpler library called requests.
-# This is external library that you may need to install first.
 import requests
 
 
 def get_data():
     # With requests, we can ask the web service for the data.
-    # Can you understand the parameters we are passing here?
+    # The parameters define our query:
+    # - starttime/endtime: date range for earthquakes
+    # - minlatitude/maxlatitude: bounding box for latitude (50.008 to 58.723)
+    # - minlongitude/maxlongitude: bounding box for longitude (-9.756 to 1.67)
+    # - minmagnitude: minimum magnitude of earthquakes to include
+    # - orderby: sort by time ascending
     response = requests.get(
         "http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
         params={
@@ -24,38 +25,54 @@ def get_data():
     # The response we get back is an object with several fields.
     # The actual contents we care about are in its text field:
     text = response.text
-    # To understand the structure of this text, you may want to save it
-    # to a file and open it in VS Code or a browser.
-    # See the README file for more information.
-    ...
-
-    # We need to interpret the text to get values that we can work with.
-    # What format is the text in? How can we load the values?
-    return ...
+    
+    # The text is in JSON format, so we need to parse it to a Python dictionary
+    data = response.json()
+    
+    return data
 
 def count_earthquakes(data):
     """Get the total number of earthquakes in the response."""
-    return ...
+    # In GeoJSON format, earthquakes are stored in the "features" array
+    # The count is simply the length of this array
+    return len(data["features"])
 
 
 def get_magnitude(earthquake):
-    """Retrive the magnitude of an earthquake item."""
-    return ...
+    """Retrieve the magnitude of an earthquake item."""
+    # In the GeoJSON structure, magnitude is at:
+    # earthquake -> properties -> mag
+    return earthquake["properties"]["mag"]
 
 
 def get_location(earthquake):
     """Retrieve the latitude and longitude of an earthquake item."""
     # There are three coordinates, but we don't care about the third (altitude)
-    return ...
+    # In GeoJSON, coordinates are stored as [longitude, latitude, altitude]
+    coordinates = earthquake["geometry"]["coordinates"]
+    return coordinates[1], coordinates[0]  # Return as (latitude, longitude)
 
 
 def get_maximum(data):
     """Get the magnitude and location of the strongest earthquake in the data."""
-    ...
+    max_magnitude = -1
+    max_earthquake = None
+    
+    # Iterate through all earthquakes to find the one with maximum magnitude
+    for earthquake in data["features"]:
+        magnitude = get_magnitude(earthquake)
+        if magnitude > max_magnitude:
+            max_magnitude = magnitude
+            max_earthquake = earthquake
+    
+    # Get the location of the strongest earthquake
+    max_location = get_location(max_earthquake)
+    
+    return max_magnitude, max_location
 
 
 # With all the above functions defined, we can now call them and get the result
 data = get_data()
-print(f"Loaded {count_earthquakes(data)}")
+print(f"Loaded {count_earthquakes(data)} earthquakes")
 max_magnitude, max_location = get_maximum(data)
 print(f"The strongest earthquake was at {max_location} with magnitude {max_magnitude}")
